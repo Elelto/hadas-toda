daeca5444af59fe3ed02fff2bd8e573b4ed0bd0c
Eli <24wowled@gmail.com>
2025-06-29 09:06:19 +0300

    הוספת ניתוב לדף האדמין ב-React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -25,6 +25,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/blog/:slug" element={<BlogPost />} />
+            <Route path="/admin/*" element={<Navigate to="/admin/index.html" replace />} />
           </Routes>
         </main>
         <Footer />
