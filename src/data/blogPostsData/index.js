2220e7eb4452df47b9e7406938e18d8768411c67
Eli <24wowled@gmail.com>
2025-09-14 22:06:15 +0300

    PopUpEvryTime

--- src/data/blogPostsData/index.js
+++ src/data/blogPostsData/index.js
@@ -1,13 +1,6 @@
 // קובץ זה נוצר אוטומטית על ידי סקריפט update-blog-data.js
 // אין לערוך אותו ידנית כי השינויים יימחקו בפעם הבאה שהסקריפט ירוץ
 
-import post16 from './post16';
-import post15 from './post15';
-import post14 from './post14';
-import post13 from './post13';
-import post12 from './post12';
-import post11 from './post11';
-import post10 from './post10';
 import post9 from './post9';
 import post8 from './post8';
 import post7 from './post7';
@@ -19,13 +12,6 @@ import post2 from './post2';
 import post1 from './post1';
 
 const blogPosts = [
-  post16,
-  post15,
-  post14,
-  post13,
-  post12,
-  post11,
-  post10,
   post9,
   post8,
   post7,
